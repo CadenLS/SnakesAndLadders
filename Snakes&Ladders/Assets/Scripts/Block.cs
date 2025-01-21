029a9e1df6d5a5e109bdf22161c3c2b007789284
Kol4mo <47543776+kol4mo@users.noreply.github.com>
2025-01-21 11:47:07 -0700

    made player movement

--- Snakes&Ladders/Assets/Scripts/Block.cs
+++ Snakes&Ladders/Assets/Scripts/Block.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class block : MonoBehaviour {
+public class Block : MonoBehaviour {
     [SerializeField] public bool extraMovement =  false;
     [SerializeField] public int movement = 0; // positive for the ladder, negative for the snakes
 }
--- Snakes&Ladders/Assets/Scripts/GameManager.cs
+++ Snakes&Ladders/Assets/Scripts/GameManager.cs
@@ -1,18 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Block[] grid;
+	[SerializeField] Button rollButton;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[SerializeField] private float snappingDistance = 0.1f;
+    [SerializeField] Player[] players;
+    int currentPlayer = 0;
+
+	[SerializeField] private TextMeshProUGUI currenPlayerVisual;
+
+
+
+	private void Start() {
+		
+	}
+
+	private void Update() {
+		//handle spline movment
+		Player player = players[currentPlayer];
+		if (player.currentPosition != player.nextPosition) {
+			player.transform.position = Vector3.Lerp(player.transform.position, grid[player.nextPosition-1].transform.position, Time.deltaTime);
+			if (Vector3.Distance(player.transform.position, grid[player.nextPosition-1].transform.position) < snappingDistance) {
+				player.transform.position = grid[player.nextPosition-1].transform.position;
+				player.currentPosition = player.nextPosition;
+				//update to next player UI and code
+				currentPlayer += 1;
+				if (currentPlayer >= players.Length) {
+					currentPlayer = 0;
+				}
+				currenPlayerVisual.text = currentPlayer.ToString();
+				//enable roll button
+				rollButton.enabled = true;
+			}
+		}
+	}
+
+	public void OnRoll() {
+		//disable roll button
+		rollButton.enabled = false;
+		//get random number
+		int movement = Random.Range(1, 7);
+		//display random number
+		//move player
+		MovePlayer(movement);
+	}
+
+	private void MovePlayer(int Movement) {
+		Player player = players[currentPlayer];
+		player.nextPosition = Movement + player.currentPosition;
+		if (player.nextPosition > grid.Length) {
+			player.nextPosition = player.currentPosition;
+			//update to next player UI and code
+			currentPlayer += 1;
+			if (currentPlayer >= players.Length) {
+				currentPlayer = 0;
+			}
+			currenPlayerVisual.text = currentPlayer.ToString();
+			//enable roll button
+			rollButton.enabled = true;
+		} else if (player.nextPosition == grid.Length){
+			//win
+		}
+	}
 }
