7a8e8bd24223d0e1fc5b19dcf9a1d61b15953534
FiniteGuide9147 <158355100+FiniteGuide9147@users.noreply.github.com>
2025-01-23 09:36:25 -0700

    added backend code foulder to keep my classes together, added characterSelectionManager class, added color selection code.

---
+++ Snakes&Ladders/Backend Code/CharacterSelectionManager.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+public class CharacterSelectionManager
+{
+	public CharacterSelectionManager()
+	{
+		string redHexCode = "#ff0000";
+		string blueHexCode = "#0000ff";
+		string yellowHexCode = "#ffff00";
+		string purpleHexCode = "#9933ff";
+		string greenHexCode = "#33cc33";
+		string orangeHexCode = "#ff9933";
+        string[] choosableColors = new string[6] { redHexCode, blueHexCode, yellowHexCode, purpleHexCode, greenHexCode, orangeHexCode};
+		string[] chosenColors = new string[6];
+
+		public void colorChosen (string color, int index)
+		{
+			string colorChosen = color.toLower();
+			switch (colorchosen)
+			{
+				case "red":
+					chosenColors[index] = redHexCode;
+					break;
+				case "blue":
+					chosenColors[index] = blueHexCode;
+					break;
+				case "yellow":
+					chosenColors[index] = yellowHexCode
+						break;
+				case "purple":
+					chosenColors[index] = purpleHexCode;
+					break;
+				case "green":
+					chosenColors[index] = greenHexCode;
+					break;
+				case "orange":
+					chosenColors[index] = orangeHexCode;
+					break;
+
+			}
+		}
+    }
+}
