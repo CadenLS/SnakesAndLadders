3835161afdd5bf0eb4c56e1ba83100eab3c1854e
FiniteGuide9147 <158355100+FiniteGuide9147@users.noreply.github.com>
2025-01-23 09:51:11 -0700

    Fixed formatting issue.

--- Snakes&Ladders/Backend Code/CharacterSelectionManager.cs
+++ Snakes&Ladders/Backend Code/CharacterSelectionManager.cs
@@ -2,46 +2,48 @@
 
 public class CharacterSelectionManager
 {
-	public CharacterSelectionManager()
-	{
-		string redHexCode = "#ff0000";
-		string blueHexCode = "#0000ff";
-		string yellowHexCode = "#ffff00";
-		string purpleHexCode = "#9933ff";
-		string greenHexCode = "#33cc33";
-		string orangeHexCode = "#ff9933";
-        string[] choosableColors = new string[6] { redHexCode, blueHexCode, yellowHexCode, purpleHexCode, greenHexCode, orangeHexCode};
-		string[] chosenColors = new string[6];
+    const string redHexCode = "#ff0000";
+    const string blueHexCode = "#0000ff";
+    const string yellowHexCode = "#ffff00";
+    const string purpleHexCode = "#9933ff";
+    const string greenHexCode = "#33cc33";
+    const string orangeHexCode = "#ff9933";
+    const string[] choosableColors = new string[6] { redHexCode, blueHexCode, yellowHexCode, purpleHexCode, greenHexCode, orangeHexCode };
+    string[] chosenColors = new string[6];
 
-		
-		public void colorChosen (string color, int index)
-		{
-			string colorChosen = color.toLower();
-			switch (colorchosen)
-			{
-				case "red":
-					chosenColors[index] = redHexCode;
-					break;
-				case "blue":
-					chosenColors[index] = blueHexCode;
-					break;
-				case "yellow":
-					chosenColors[index] = yellowHexCode
-						break;
-				case "purple":
-					chosenColors[index] = purpleHexCode;
-					break;
-				case "green":
-					chosenColors[index] = greenHexCode;
-					break;
-				case "orange":
-					chosenColors[index] = orangeHexCode;
-					break;
-				case default:
-					Console.WriteLine("No Valid Color detected. Please input 'red', 'blue', 'yellow', purple', 'green', or 'orange'.");
-					break;
+    private int playerAmount;
 
-			}
-		}
+    public CharacterSelectionManager()
+    {
     }
+    public void colorChosen(string color, int index)
+    {
+        string colorChosen = color.toLower();
+        switch (colorchosen)
+        {
+            case "red":
+                chosenColors[index] = redHexCode;
+                break;
+            case "blue":
+                chosenColors[index] = blueHexCode;
+                break;
+            case "yellow":
+                chosenColors[index] = yellowHexCode
+
+                        break;
+            case "purple":
+                chosenColors[index] = purpleHexCode;
+                break;
+            case "green":
+                chosenColors[index] = greenHexCode;
+                break;
+            case "orange":
+                chosenColors[index] = orangeHexCode;
+                break;
+            case default:
+                Console.WriteLine("No Valid Color detected. Please input 'red', 'blue', 'yellow', purple', 'green', or 'orange'.");
+                break;
+        }
+    }
+
 }
