2b7a0bafe5df458eed5c4cf07cd79ed1b2f2fec7
CadenLS <csinclair@student.neumont.edu>
2025-01-23 10:23:42 -0700

    Dice Roll

--- Snakes&Ladders/Assets/Scripts/GameManager.cs
+++ Snakes&Ladders/Assets/Scripts/GameManager.cs
@@ -9,19 +9,21 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour {
 	[SerializeField] private Block[] grid;
 	[SerializeField] Button rollButton;
+    [SerializeField] private DiceRoller2D diceRoller;
 
-	[SerializeField] private float snappingDistance = 0.1f;
+    [SerializeField] private float snappingDistance = 0.1f;
 	[SerializeField] Player[] players;
 	int currentPlayer = 0;
 
 	[SerializeField] private TextMeshProUGUI currenPlayerVisual;
-	[SerializeField] private float moveSpeed = 1;
+	[SerializeField] private float moveSpeed = 4;
 	bool splineMovment = false;
 	int pointIndex = 0;
 
-	private void Start() {
-
-	}
+	private void Start() 
+	{
+        diceRoller.OnRoll += HandleDiceRoll;
+    }
 
 	private void Update() {
 		Player player = players[currentPlayer];
@@ -62,31 +64,73 @@ public class GameManager : MonoBehaviour {
 		}
 	}
 
-	public void OnRoll() {
-		//disable roll button
-		rollButton.enabled = false;
-		//get random number
-		int movement = Random.Range(1, 7);
-		//display random number
-		//move player
-		MovePlayer(movement);
-	}
+    public void OnRoll()
+    {
+        // Disable roll button
+        rollButton.enabled = false;
 
-	private void MovePlayer(int Movement) {
-		Player player = players[currentPlayer];
-		player.nextPosition = Movement + player.currentPosition;
-		if (player.nextPosition > grid.Length) {
-			player.nextPosition = player.currentPosition;
-			//update to next player UI and code
-			currentPlayer += 1;
-			if (currentPlayer >= players.Length) {
-				currentPlayer = 0;
-			}
-			currenPlayerVisual.text = currentPlayer.ToString();
-			//enable roll button
-			rollButton.enabled = true;
-		} else if (player.nextPosition == grid.Length) {
-			//win
-		}
-	}
+        // Trigger the dice roll
+        diceRoller.RollDice();
+    }
+
+    private void HandleDiceRoll(int movement)
+    {
+        MovePlayer(movement);
+    }
+
+    private void MovePlayer(int movement)
+    {
+        Player player = players[currentPlayer];
+        player.nextPosition = movement + player.currentPosition;
+
+        if (player.nextPosition > grid.Length)
+        {
+            player.nextPosition = player.currentPosition;
+            AdvanceToNextPlayer();
+        }
+        else if (player.nextPosition == grid.Length)
+        {
+            Debug.Log($"Player {currentPlayer + 1} wins!");
+        }
+    }
+
+    private void AdvanceToNextPlayer()
+    {
+        currentPlayer++;
+        if (currentPlayer >= players.Length)
+        {
+            currentPlayer = 0;
+        }
+
+        currenPlayerVisual.text = currentPlayer.ToString();
+        rollButton.enabled = true; // Enable roll button
+    }
+
+    //public void OnRoll() {
+    //	//disable roll button
+    //	rollButton.enabled = false;
+    //	//get random number
+    //	int movement = Random.Range(1, 7);
+    //	//display random number
+    //	//move player
+    //	MovePlayer(movement);
+    //}
+
+    //private void MovePlayer(int Movement) {
+    //	Player player = players[currentPlayer];
+    //	player.nextPosition = Movement + player.currentPosition;
+    //	if (player.nextPosition > grid.Length) {
+    //		player.nextPosition = player.currentPosition;
+    //		//update to next player UI and code
+    //		currentPlayer += 1;
+    //		if (currentPlayer >= players.Length) {
+    //			currentPlayer = 0;
+    //		}
+    //		currenPlayerVisual.text = currentPlayer.ToString();
+    //		//enable roll button
+    //		rollButton.enabled = true;
+    //	} else if (player.nextPosition == grid.Length) {
+    //		//win
+    //	}
+    //}
 }
